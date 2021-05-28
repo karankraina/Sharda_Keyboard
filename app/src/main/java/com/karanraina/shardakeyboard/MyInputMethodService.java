b69df819e95f37669853592e4d1f1789eaa483e8
Karan Raina <karanraina1996@gmail.com>
2021-05-28 23:10:30 +0530

    First Commit

---
+++ app/src/main/java/com/karanraina/shardakeyboard/MyInputMethodService.java
@@ -0,0 +1,107 @@
+package com.karanraina.shardakeyboard;
+
+import android.content.res.Resources;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.KeyboardShortcutGroup;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView keyboardView;
+    private Keyboard keyboard1;
+    private Keyboard keyboard2;
+
+    private boolean caps = false;
+
+    @Override
+    public View onCreateInputView() {
+        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        keyboard1 = new Keyboard(this, R.xml.keys_layout_1);
+        keyboard2 = new Keyboard(this, R.xml.keys_layout_2);
+        keyboardView.setKeyboard(keyboard1);
+        keyboardView.setPreviewEnabled(true);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+    }
+
+    public static int getScreenWidth() {
+        return Resources.getSystem().getDisplayMetrics().widthPixels;
+    }
+
+
+    @Override
+    public void onText(CharSequence text) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        if (inputConnection != null) {
+            String textToInput = (String) text;
+            switch(textToInput) {
+                case "KEYCODE_DELETE" :
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(2, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+                    break;
+                case "KEYCODE_SHIFT":
+                    if(caps) {
+                        keyboardView.setKeyboard(keyboard2);
+                    } else {
+                        keyboardView.setKeyboard(keyboard1);
+                    }
+                    caps = !caps;
+                    break;
+                case "KEYCODE_DONE":
+                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+
+                    break;
+                default :
+                    inputConnection.commitText(textToInput, 1);
+
+            }
+        }
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
